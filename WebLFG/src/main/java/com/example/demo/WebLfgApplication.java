2232894c4a7621e69e13a8165c6a89bc12315ecc
Nayara <nay.nivea@gmail.com>
2023-12-05 17:52:21 +0100

    Inicio

---
+++ WebLFG/src/main/java/com/example/demo/WebLfgApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLfgApplication.class, args);
+	}
+
+}
